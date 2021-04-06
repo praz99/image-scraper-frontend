5c8b8064490380a1403014bbf3835a9e43a8769d
Praz <t.prazwal@gmail.com>
2021-04-06 03:07:41 +0200

    fix the eslint errors

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React,  { useState } from 'react';
+/* eslint-disable max-len */
+
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const App = () => {
@@ -12,36 +14,39 @@ const App = () => {
 
   const { url } = state;
 
-  const addhttp = url => {
+  const addhttp = (url) => {
+    let urlC = url;
     if (!/^(?:f|ht)tps?:\/\//.test(url)) {
-      url = "https://" + url;
+      urlC = `https://${url}`;
     }
-    return url;
+    return urlC;
   };
 
   return (
     <>
-    <div className="App">
-      <h1>Grab the images</h1>
-      <div className="input-container">
-        <label htmlFor="url-input">Enter the URL</label>
-        <input
-          placeholder="http://www.example.com"
-          className="url-input"
-          id="url-input"
-          type="url"
-          name="url"
-          value={url}
-          onChange={handleChange}
-        />
-        <Link to={`/images?url=${addhttp(url)}`}>Go</Link>
+      <div className="App">
+        <h1>Grab the images</h1>
+        <div className="input-container">
+          <p>Enter the URL</p>
+          <input
+            placeholder="http://www.example.com"
+            className="url-input"
+            id="url-input"
+            type="url"
+            name="url"
+            value={url}
+            onChange={handleChange}
+          />
+          <Link to={`/images?url=${addhttp(url)}`}>Go</Link>
+        </div>
       </div>
-    </div>
-    <div className="input-info">
-      <p>Grab the images is a tool that fetches all the images that are present in a webpage. Enter the URL of the webpage in the input field and press the Go button.</p>
-    </div>
+      <div className="input-info">
+        <p>Grab the images is a tool that fetches all the images that are present in a webpage. Enter the URL of the webpage in the input field and press the Go button.</p>
+      </div>
     </>
   );
-}
+};
 
 export default App;
+
+/* eslint-enable max-len */
--- src/components/Images.js
+++ src/components/Images.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
 
 const useQuery = () => new URLSearchParams(useLocation().search);
 
@@ -9,41 +10,35 @@ const Images = () => {
   const [images, setImages] = useState([]);
   const [message, setMessage] = useState('');
   const query = useQuery();
-  let url = query.get('url');
+  const url = query.get('url');
 
   useEffect(() => {
     const fetchImages = async () => {
-      const result = await axios.post('http://localhost:3001/scrape', { url })
-      console.log(result);
+      const result = await axios.post('http://localhost:3001/scrape', { url });
       if (result.data.images) {
         setImages(result.data.images);
       } else {
-        console.log(result.data.message);
         setMessage(result.data.message);
       }
     };
     fetchImages();
   }, [url]);
 
-  const cleanImages = images.filter(img => {
-    return img;
-  });
-
-  console.log(cleanImages);
+  const cleanImages = images.filter((img) => img);
 
   return (
     <>
-    <div className="images">
-      <Link to="/" className="home-button">Home</Link>
-      {message && <div className="message-box"><p className="message">{message}</p></div>}
-      <div className="images-container">
-        {cleanImages.map((img, index) => (
-          <img className="image-single" key={index} src={img} alt="..." />
-        ))}
+      <div className="images">
+        <Link to="/" className="home-button">Home</Link>
+        {message && <div className="message-box"><p className="message">{message}</p></div>}
+        <div className="images-container">
+          {cleanImages.map((img) => (
+            <img className="image-single" key={uuidv4()} src={img} alt="..." />
+          ))}
+        </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
 export default Images;
